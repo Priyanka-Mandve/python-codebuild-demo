1fc7553da462d6e994a26339038641253847aa6f
Priyanka-Mandve <priyankamandve1@gmail.com>
2026-06-11 10:21:26 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+# Create a list of 10 numbers
+numbers = [10, 25, 35, 40, 55, 60, 75, 80, 90, 100]
+
+# Find highest number
+highest = max(numbers)
+
+# Find lowest number
+lowest = min(numbers)
+
+# Find average
+average = sum(numbers) / len(numbers)
+
+# Display results
+print("List of Numbers:", numbers)
+print("Highest Number:", highest)
+print("Lowest Number:", lowest)
+print("Average:", average)
